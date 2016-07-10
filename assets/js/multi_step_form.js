0f1c7a6caa1c6c7ffca64e8e6c1a59e92bd705ab
AlexRamey <alex.b.ramey@gmail.com>
2016-07-09 19:59:01 -0700

    Removed Form Validation

--- assets/js/multi_step_form.js
+++ assets/js/multi_step_form.js
@@ -1,33 +1,3 @@
-/*------------Validation Function-----------------*/
-var count = 0; // To count blank fields.
-function validation(event) {
-var radio_check = document.getElementsByName('gender'); // Fetching radio button by name.
-var input_field = document.getElementsByClassName('text_field'); // Fetching all inputs with same class name text_field and an html tag textarea.
-var text_area = document.getElementsByTagName('textarea');
-// Validating radio button.
-if (radio_check[0].checked == false && radio_check[1].checked == false) {
-var y = 0;
-} else {
-var y = 1;
-}
-// For loop to count blank inputs.
-for (var i = input_field.length; i > count; i--) {
-if (input_field[i - 1].value == '' || text_area.value == '') {
-count = count + 1;
-} else {
-count = 0;
-}
-}
-if (count != 0 || y == 0) {
-alert("*All Fields are mandatory*"); // Notifying validation
-event.preventDefault();
-} else {
-return true;
-}
-}
-
-function serializeResponses
-
 /*---------------------------------------------------------*/
 // Function that executes on click of first next button.
 function next_step1() {
